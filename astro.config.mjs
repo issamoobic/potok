e2be73dee27bd48ebe84c84a0996c9bcba3f111c
Sasha <issamoobic@email.com>
2026-05-13 11:32:49 +0300

    fix vercel build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://potok.ai',
   output: 'server',
-  adapter: node({ mode: 'standalone' }),
-  integrations: [tailwind(), mdx(), sitemap()],
+  adapter: vercel(),
+  integrations: [tailwind(), mdx()],
 });
